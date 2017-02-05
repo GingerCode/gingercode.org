5c9b10d8ccfa5802aaca1bebcd81a23417387eb8
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-02-05 19:15:52 +0000

    lint fix

--- test/spec.js
+++ test/spec.js
@@ -5,12 +5,11 @@ beforeEach(function () {
     jasmine.addMatchers(require('jasmine-diff-matchers').diffChars);
 });
 
-
 describe("General Conversion", function() {
-  var full_features_ginger = fs.readFileSync('./test/asserts/full_features.ginger', 'utf8');
-  var full_features_js = fs.readFileSync('./test/asserts/full_features.js', 'utf8');
+    var full_features_ginger = fs.readFileSync('./test/asserts/full_features.ginger', 'utf8');
+    var full_features_js = fs.readFileSync('./test/asserts/full_features.js', 'utf8');
   
-  it("Full example", function() {
-            expect(gingercode.compile(full_features_ginger)).diffChars(full_features_js);
-  });
+    it("Full example", function() {
+        expect(gingercode.compile(full_features_ginger)).diffChars(full_features_js);
+    });
 });
